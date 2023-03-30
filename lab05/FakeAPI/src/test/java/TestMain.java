0a6e01e76f776b2f75d9213a1a94c64b0a8e385c
Jakub Krzynowek <kubaisnow@gmail.com>
2023-03-30 20:12:05 +0200

    lab05 fin

--- lab05/FakeAPI/src/test/java/TestMain.java
+++ lab05/FakeAPI/src/test/java/TestMain.java
@@ -13,26 +13,18 @@ import static org.mockito.Mockito.when;
     {"age":62,"count":298219,"name":"michael"}
 
     https://api.agify.io?name[]=michael&name[]=matthew&name[]=jane
+    [{"age":62,"count":298219,"name":"michael"},{"age":44,"count":53379,"name":"matthew"},{"age":68,"count":40935,"name":"jane"}]
 
     https://api.agify.io?name=michael&country_id=US
+    {"age":55,"count":108496,"country_id":"US","name":"michael"}
 
+    https://api.agify.io?name=peter&apikey=1234
     401 - Unauthorized
     { "error": "Invalid API key" }
 
-    402 - Payment Required
-    { "error": "Subscription is not active" }
-
+    https://api.agify.io
     422 - Unprocessable Entity
     { "error": "Missing 'name' parameter" }
-
-    422 - Unprocessable Entity
-    { "error": "Invalid 'name' parameter" }
-
-    429 - Too Many Requests
-    { "error": "Request limit reached" }
-
-    429 - Too Many Requests
-    { "error": "Request limit too low to process request" }
 
  */
 public class TestMain {
@@ -41,18 +33,45 @@ public class TestMain {
     @Test
     public void simpleTest() {
         Main main = mock(Main.class);
-        when(main.name("Jakub")).thenReturn("Kuba");
+        when(main.name("Jakub")).thenReturn("{\"age\":34,\"count\":36211,\"name\":\"Jakub\"}");
         x = main.name("Jakub");
 
-        assertEquals(x, "Kuba");
+        assertEquals(x, "{\"age\":34,\"count\":36211,\"name\":\"Jakub\"}");
+    }
+
+    @Test
+    public void simpleTest3() {
+        Main main = mock(Main.class);
+        when(main.name("name[]=michael&name[]=matthew&name[]=jane")).thenReturn("[{\"age\":62,\"count\":298219,\"name\":\"michael\"},{\"age\":44,\"count\":53379,\"name\":\"matthew\"},{\"age\":68,\"count\":40935,\"name\":\"jane\"}]");
+        x = main.name("name[]=michael&name[]=matthew&name[]=jane");
+
+        assertEquals(x, "[{\"age\":62,\"count\":298219,\"name\":\"michael\"},{\"age\":44,\"count\":53379,\"name\":\"matthew\"},{\"age\":68,\"count\":40935,\"name\":\"jane\"}]");
+    }
+
+    @Test
+    public void simpleTest5() {
+        Main main = mock(Main.class);
+        when(main.name("https://api.agify.io?name=michael&country_id=US")).thenReturn("{\"age\":55,\"count\":108496,\"country_id\":\"US\",\"name\":\"michael\"}");
+        x = main.name("https://api.agify.io?name=michael&country_id=US");
+
+        assertEquals(x, "{\"age\":55,\"count\":108496,\"country_id\":\"US\",\"name\":\"michael\"}");
+    }
+
+    @Test
+    public void simpleTest4() {
+        Main main = mock(Main.class);
+        when(main.name("https://api.agify.io?name=peter&apikey=1234")).thenReturn("{\"error\":\"Invalid API key\"}");
+        x = main.name("https://api.agify.io?name=peter&apikey=1234");
+
+        assertEquals(x, "{\"error\":\"Invalid API key\"}");
     }
 
     @Test
     public void simpleTest2() {
         Main main = mock(Main.class);
-        when(main.name("Jakub")).thenReturn("Kuba");
-        x = main.name("Jakub");
+        when(main.name("")).thenReturn("{ \"error\": \"Missing 'name' parameter\" }");
+        x = main.name("");
 
-        assertEquals(x, "Kuba");
+        assertEquals(x, "{ \"error\": \"Missing 'name' parameter\" }");
     }
 }
